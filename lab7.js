e73c5a9537aa619e0f381c0248a194eeb0e54aa3
Nada Hassan <nadahassan013@gmail.com>
2016-04-02 16:43:37 -0400

    lab 7 updated

--- lab7.js
+++ lab7.js
@@ -1,33 +1,41 @@
-function cToF(celsius)
+function cToF(cTemp)
 {
-  var cTemp = 45;
   var cToFahr = cTemp * 9 / 5 + 32;
   var cmessage = cTemp+'degrees C is ' + cToFahr + ' degrees F.';
-    console.log(cmessage);
+  console.log(cmessage);
 };
 
-function fToC(fahrenheit)
+function fToC(fTemp)
 {
-  var fTemp = 30;
   var fToCel = (fTemp - 32) * 5 / 9;
-  var fmessage = fTemp+'degreesF is ' + fToCel + 'degreesC.';
+  var fmessage = fTemp+'degrees F is ' + fToCel + 'degrees C.';
     console.log(fmessage);
 };
 
-fToC();
-cToF();
+// fToC();
+// cToF();
+
+
 
 
 function convert () {
-  var fmessage = fTemp+'degreesF is ' + fToCel + 'degreesC.';
-  console.log(fmessage);
-  var fmessage = document.getElementById('temp').value;
-  var conversion = Number(fmessage);
 
-  var cmessage = cTemp+'degrees C is ' + cToFahr + ' degrees F.';
-  console.log(cmessage);
-  var cmessage = document.createElement('cels');
-  tc.innerHTML = element.cels;
+  var fmessage = document.getElementById('fTemperature').value;
+
+  if (fmessage !== "") {
+    var conversion = Number(fmessage);
+    fToC(conversion);
+  }
+
+  var cmessage = document.getElementById('cTemperature').value;
+
+  if (cmessage !== "") {
+    var conversion = Number(cmessage);
+    cToF(conversion);
+  }
+
+
+
+  // var cmessage = document.createElement('p'); //p needs to be an html element
+  // cmessage.innerHTML = element.cmessage;
 };
-
-convert ();
